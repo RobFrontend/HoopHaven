e0e70f790cab2f03b6456e0acc7b242b4925e64f
RobFrontend <robert.grabowski97@gmail.com>
2024-07-16 21:45:33 +0200

    revalidate

--- app/components/Courts.js
+++ app/components/Courts.js
@@ -1,13 +1,15 @@
 import { getCourts } from "../lib/data-service";
 import CourtCard from "./CourtCard";
 
+export const revalidate = 3600;
+
 async function Courts() {
   const courts = await getCourts();
   if (!courts.length) return null;
   return (
     <div>
       <h2 className="mb-10">Discover Our Extraordinary Basketball Courts</h2>
-      <div className="grid grid-cols-2 gap-12 mb-12">
+      <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-12 mb-12">
         {courts.map((court) => (
           <CourtCard court={court} key={court.id} />
         ))}
