38af59ede5dc21cabb47d01b5c9d4247d1a245cd
RobFrontend <robert.grabowski97@gmail.com>
2024-07-18 22:22:12 +0200

    responsive

--- app/components/Courts.js
+++ app/components/Courts.js
@@ -8,7 +8,7 @@ async function Courts() {
   if (!courts.length) return null;
   return (
     <div>
-      <h2 className="mb-10 max-sm:text-2xl max-md:text-3xl">
+      <h2 className="mb-8 max-sm:text-2xl max-md:text-3xl max-md:mb-6">
         Discover Our Extraordinary Basketball Courts
       </h2>
       <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-12 max-md:gap-8 mb-12 max-md:grid-cols-2">
