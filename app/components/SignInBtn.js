9c2b2c26aa6953fecb8f3a1c159af67da8685ab6
RobFrontend <robert.grabowski97@gmail.com>
2024-12-10 02:06:32 +0100

    next auth

--- app/components/SignInBtn.js
+++ app/components/SignInBtn.js
@@ -6,11 +6,11 @@ function SignInBtn() {
   return (
     <form action={signInAction}>
       <button className=" border-neutral-300 border-2 p-4 flex items-center gap-1 hover:-translate-y-[4px] active:-translate-y-[2px] active:shadow-neutral-300/10 active:shadow-lg transition-all duration-300">
-        <p className="text-2xl">Log in with </p>
+        <p className="text-2xl max-[375px]:text-lg">Log in with </p>
         <Image
           src={GoogleLogoImg}
           quality={100}
-          className="max-h-7 w-auto self-start"
+          className="max-h-7 w-auto self-start max-[375px]:max-h-6"
           alt="google logo"
         />
       </button>
--- app/login/page.js
+++ app/login/page.js
@@ -9,7 +9,7 @@ function page() {
     <div className="py-16 max-2xl:px-8">
       <div className="pb-10">
         <h1 className="text-neutral-300 max-sm:text-6xl max-md:text-7xl">
-          Please log in
+          Sign in
         </h1>
         <div className="grid justify-center p-8 gap-4">
           <p className="text-lg max-sm:text-xs max-md:text-sm">
